473dc137d5b314fc3378218aec0a7f5d4b0af098
Gian Grassi <giangr21@gmail.com>
2021-09-08 21:13:17 -0300

    pagination

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,11 @@ import { Button } from '@chakra-ui/react';
 interface PaginationItemProps {
   number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void;
 }
 
 export function PaginationItem({
+  onPageChange,
   isCurrent = false,
   number,
 }: PaginationItemProps) {
@@ -35,6 +37,7 @@ export function PaginationItem({
       _hover={{
         bg: 'gray.500',
       }}
+      onClick={() => onPageChange(number)}
     >
       {number}
     </Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useBreakpointValue,
   IconButton,
+  Spinner,
 } from '@chakra-ui/react';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
 import Link from 'next/link';
@@ -21,8 +22,13 @@ import Link from 'next/link';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
+import { useUsers } from '../../services/hooks/useUsers';
+import { useState } from 'react';
 
 export default function UserList() {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -39,6 +45,9 @@ export default function UserList() {
           <Flex mb="8" justifyContent="space-between" alignItems="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -54,47 +63,67 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" w="8">
-                  <Checkbox colorScheme="purple" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th textAlign="end"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter usuarios</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" w="8">
+                      <Checkbox colorScheme="purple" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th textAlign="end"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="purple" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gian Grassi</Text>
-                    <Text fontSize="small" color="gray.300">
-                      giangr21@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril de 2021</Td>}
-                <Td textAlign="end">
-                  <IconButton
-                    aria-label="Edit user"
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    icon={<Icon as={RiEditLine} fontSize="16" />}
-                  />
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+                <Tbody>
+                  {data.users.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="purple" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="small" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td textAlign="end">
+                          <IconButton
+                            aria-label="Edit user"
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            icon={<Icon as={RiEditLine} fontSize="16" />}
+                          />
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
